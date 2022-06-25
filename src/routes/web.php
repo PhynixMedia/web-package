ae20a1803a26889118673f9558339cd03c2d457f
ODAdebayo <58173405+ODAdebayo@users.noreply.github.com>
2022-06-25 15:35:54 +0100

    changes to app - route

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,46 +3,17 @@
 Route::group(['middleware' => 'web'], function ()
 {
 
-    Route::group(['prefix' => 'appointment'], function ()
+    Route::group(['prefix' => 'create'], function ()
     {
-        Route::get('/', 'Web\App\Controllers\AppointmentsController@index')->name('appointment.index');
-        Route::post('/store', 'Web\App\Controllers\AppointmentsController@store')->name('appointment.store');
-    });
-
-    Route::group(['prefix' => 'callback'], function ()
-    {
-        Route::get('/', 'Web\App\Controllers\CallBackController@index')->name('callback.index');
-        Route::post('/store', 'Web\App\Controllers\CallBackController@store')->name('callback.store');
-
-    });
-
-    Route::group(['prefix' => 'contact'], function ()
-    {
-        Route::get('/', 'Web\App\Controllers\ContactsController@index')->name('contact.index');
-        Route::post('/store', 'Web\App\Controllers\ContactsController@store')->name('contact.store');
+        Route::post('/appointment', 'Web\App\Controllers\AppointmentsController@store')->name('appointment.store');
+        Route::post('/callback', 'Web\App\Controllers\CallBackController@store')->name('callback.store');
+        Route::post('/contact', 'Web\App\Controllers\ContactsController@store')->name('contact.store');
+        Route::post('/feedback', 'Web\App\Controllers\FeedBacksController@store')->name('feedback.store');
+        Route::post('/newsletter', 'Web\App\Controllers\NewslettersController@store')->name('newsletter.store');
+        Route::post('/quote', 'Web\App\Controllers\QuotesController@store')->name('quote.store');
 
     });
 
-    Route::group(['prefix' => 'feedback'], function ()
-    {
-        Route::get('/', 'Web\App\Controllers\FeedBacksController@index')->name('feedback.index');
-        Route::post('/store', 'Web\App\Controllers\FeedBacksController@store')->name('feedback.store');
-
-    });
-
-    Route::group(['prefix' => 'newsletter'], function ()
-    {
-        Route::get('/', 'Web\App\Controllers\NewslettersController@index')->name('newsletter.index');
-        Route::post('/store', 'Web\App\Controllers\NewslettersController@store')->name('newsletter.store');
-
-    });
-
-    Route::group(['prefix' => 'quote'], function ()
-    {
-        Route::get('/', 'Web\App\Controllers\QuotesController@index')->name('quote.index');
-        Route::post('/store', 'Web\App\Controllers\QuotesController@store')->name('quote.store');
-
-    });
     /**
      *      GENERAL WEB ROUTES                                      */
     Route::get('/', 'Web\App\Controllers\IndexController@index');
